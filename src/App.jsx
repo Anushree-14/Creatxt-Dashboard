2afcbde6b476e087b7b8ee93873bb19968345321
Anushree patil <anushreepatil457@gmail.com>
2025-06-23 23:44:35 +0530

    Resolved merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from "react";
-import { Routes, Route } from "react-router-dom";
+import { HAshROuter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 const Dashboard = lazy(() => import("./pages/Dashboard"));
@@ -11,11 +11,13 @@ function App() {
     <>
       <Navbar />
       <Suspense fallback={<div className="container">Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/reports" element={<Reports />} />
-        </Routes>
+        <HASHROUTER>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/reports" element={<Reports />} />
+          </Routes>
+        </HASHROUTER>
       </Suspense>
     </>
   );

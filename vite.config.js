c9d14342033d8978d2a61555f7572fc910780e81
Anushree patil <anushreepatil457@gmail.com>
2025-06-25 15:11:36 +0530

    f init

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Creatxt-Dashboard",
-})
+  optimizeDeps: {
+    entries: [], // Optional: specify entry files to pre-bundle
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'EMFILE') return; // Suppress too many open files warning
+        warn(warning);
+      },
+    },
+  },
+  worker: {
+    format: 'es',
+  },
+});
+
+
